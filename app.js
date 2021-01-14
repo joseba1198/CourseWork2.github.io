b533fbf460121fdad0c1f8cf19d73f4af67667ab
joseba vazquez <joseph.yoyoitsjojo123@gmail.com>
2021-01-14 19:02:43 +0000

    static file middleware

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 
 //Logger Middleware
@@ -9,6 +10,20 @@ app.use(function(request,response, next){
     next(); //Next middleware
 
 })
+
+//static image middleware
+app.use(function(req, res, next) {
+    var filePath = path.join(__dirname, "public" , req.url); // finds the path that the image files are.
+    fs.stat(filePath, function(err, fileInfo) { // info about the file
+    if (err) {
+    next();
+    return;
+    }
+    if (fileInfo.isFile()) res.sendFile(filePath);
+    else next();
+    });
+    });
+    
 
 app.listen(3000, () => {
     console.log("app listens on port 3000");
